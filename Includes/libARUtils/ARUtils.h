4484b1bc784ca58581fe4877d407f22a0cda098e
david.flattin <david.flattin.ext@parrot.com>
2014-01-06 12:03:56 +0100

    Global include file

---
+++ Includes/libARUtils/ARUtils.h
@@ -0,0 +1,16 @@
+/**
+ * @file ARUtils.h
+ * @brief libARUtils header file. Global include file.
+ * @date 30/12/2013
+ * @author david.flattin.ext@parrot.com
+ **/
+
+#ifndef _ARUTILS_H_
+#define _ARUTILS_H_
+
+#include <libARUtils/ARUTILS_Error.h>
+#include <libARUtils/ARUTILS_Ftp.h>
+#include <libARUtils/ARUTILS_FileSystem.h>
+
+#endif /* _ARUTILS_H_ */
+
