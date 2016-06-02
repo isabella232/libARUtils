796b5e00e52dc974cb2d425c590749780276a653
Maxime MAITRE <maxime.maitre@parrot.com>
2016-06-02 14:25:54 +0200

    [BUILD] Use Android.mk generated by alchemy.
    Change-Id: Iab7341952c07b6942d9c352e9b83a1e5e74b3600
    Reviewed-on: https://gerrit.parrot.biz/40051
    Reviewed-by: Djavan Bertrand <djavan.bertrand@parrot.com>

--- Android.mk
+++ Android.mk
@@ -5,7 +5,16 @@ include $(CLEAR_VARS)
 
 LOCAL_CFLAGS := -g
 LOCAL_MODULE := libarutils_android
-LOCAL_SRC_FILES := JNI/c/ARUTILS_JNI_RFCommFtp.c JNI/c/ARUTILS_JNI_FileSystem.c JNI/c/ARUTILS_JNI_BLEFtp.c JNI/c/ARUTILS_JNI_Manager.c
+LOCAL_SRC_FILES := \
+	JNI/c/ARUTILS_JNI_RFCommFtp.c \
+	JNI/c/ARUTILS_JNI_FileSystem.c \
+	JNI/c/ARUTILS_JNI_BLEFtp.c \
+	JNI/c/ARUTILS_JNI_Manager.c
+
 LOCAL_LDLIBS := -llog -lz
-LOCAL_SHARED_LIBRARIES := libARUtils-prebuilt libARSAL-prebuilt libmux-prebuilt
+LOCAL_SHARED_LIBRARIES := \
+	libARUtils \
+	libARSAL \
+	libmux
+
 include $(BUILD_SHARED_LIBRARY)
